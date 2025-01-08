4129d52bff29c407a0c283f8e9d4e4d784800e42
sakshi-bhujbal <bhujbalsakshi510@gmail.com>
2025-01-08 18:02:13 +0530

    commit

--- Blog/Routes/blog.js
+++ Blog/Routes/blog.js
@@ -1,9 +1,24 @@
 const express=require("express");
 const router=express.Router();
 const path=require("path")
+const blog=require("../data/blogs")
 
 router.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname,'../templates/index.html'))
+})
+
+router.get('/blog',(req,res)=>{
+    blog.forEach((e)=>{
+        console.log(e.title+" "+e.Content);
+    });
+    res.sendFile(path.join(__dirname,'../templates/blogHome.html'))
+})
+
+router.get('/blogpost/:slug',(req,res)=>{
+    myblog=blog.filter((e)=>{
+        return e.slug==req.params.slug
+    });
+    res.sendFile(path.join(__dirname,'../templates/blogPage.html'))
 })
 
 module.exports=router;
---
+++ Blog/data/blogs.js
@@ -0,0 +1,21 @@
+
+const blogs=[
+        {
+            title:"How to get started with Express",
+            Content:"Express.js is a minimal and flexible Node.js web application framework that provides a list of features for building web and mobile applications easily. It simplifies the development of server-side applications by offering an easy-to-use API for routing, middleware, and HTTP utilities.",
+            slug:"Express"
+        },
+        {
+            title:"How to get started with React",
+            Content:"React Guide.",
+             slug:"React"
+        },
+        {
+            title:"How to get started with MongoDB",
+            Content:"Getting started with mongoDb",
+             slug:"MongoDB"
+        },
+    ]
+
+
+module.exports=blogs;
