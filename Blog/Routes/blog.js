b6403d35df1c43b2f371a4a3ba6a16decdaf7d14
sakshi-bhujbal <bhujbalsakshi510@gmail.com>
2025-01-09 10:06:54 +0530

    commit

--- Blog/Routes/blog.js
+++ Blog/Routes/blog.js
@@ -4,21 +4,21 @@ const path=require("path")
 const blog=require("../data/blogs")
 
 router.get('/',(req,res)=>{
-    res.sendFile(path.join(__dirname,'../templates/index.html'))
+    res.sendFile(path.join(__dirname,'../Views/index.html'))
 })
 
 router.get('/blog',(req,res)=>{
     blog.forEach((e)=>{
         console.log(e.title+" "+e.Content);
     });
-    res.sendFile(path.join(__dirname,'../templates/blogHome.html'))
+    res.sendFile(path.join(__dirname,'../Views/blogHome.html'))
 })
 
 router.get('/blogpost/:slug',(req,res)=>{
     myblog=blog.filter((e)=>{
         return e.slug==req.params.slug
     });
-    res.sendFile(path.join(__dirname,'../templates/blogPage.html'))
+    res.sendFile(path.join(__dirname,'../Views/blogPage.html'))
 })
 
 module.exports=router;
